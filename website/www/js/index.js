1281e2b4aa361fba354d29efcceb01c2e2d5e2b0
tejasprabhune <tejas.prabhune@gmail.com>
2022-12-23 10:31:48 -0800

    Add comments

--- website/www/js/index.js
+++ website/www/js/index.js
@@ -1,11 +1,12 @@
+// import Algolia app-id and api-key and initiliaze index with all records
 import { getAppID, getAPIKey } from "./config.js";
 const client = algoliasearch(getAppID(), getAPIKey());
 const index = client.initIndex("all_rounds");
 
-var titleLink = document.getElementById("title-link");
-var title = document.getElementById("h1-title");
-var inputQuery = document.getElementById("input-search-bar");
 
+// initialize document elements
+var titleLink = document.getElementById("title-link");
+var inputQuery = document.getElementById("input-search-bar");
 
 var previewsWrapper = document.getElementById("div-results");
 var searchBar = document.getElementById("div-search-bar");
@@ -13,9 +14,41 @@ var searchBar = document.getElementById("div-search-bar");
 var resultsWrapper = document.getElementById("results-wrapper");
 var currentResultWrapper = document.getElementById("current-result");
 
-var currentSearchResults;
+// On page load
+window.addEventListener("load", onPageLoad);
 
-window.addEventListener("load", function() {
+// To reload page on title click
+titleLink.addEventListener("click", reload);
+
+// Track user input in search bar for previews
+inputQuery.addEventListener("input", function() {
+    if(this.value) {
+        searchDisplayPreviews(this.value);
+    } else {
+        resetSearchBar(true);
+    }
+}, false);
+
+// Generate and display result cards when pressed Enter in search bar
+inputQuery.addEventListener("keypress", function(e) {
+    if(e.key == "Enter") {
+        displayGenResults();
+    }
+});
+
+// Box shadow on focus for search bar
+inputQuery.addEventListener("focus", function () {
+    searchBar.style["box-shadow"] = "rgba(0, 0, 0, 0.24) 1px 1px 8px";
+});
+
+inputQuery.addEventListener("focusout", function () {
+    searchBar.style["box-shadow"] = "rgba(99, 99, 99, 0.2) 0px 0px 8px 0px";
+});
+
+/**
+ * Resets results, generates random 10 cards and displays results
+ */
+function onPageLoad() {
     resultsWrapper.innerHTML = "";
     let pageNum = Math.floor(Math.random() * 50);
 
@@ -26,55 +59,93 @@ window.addEventListener("load", function() {
         generateResultCards(hits, resultsWrapper);
     });
     resetSearchBar();
-});
+}
 
-titleLink.addEventListener("click", function() {
+/**
+ * Reloads page to original state
+ */
+function reload() {
     window.open("index.html", "_self");
-});
+}
 
-inputQuery.addEventListener("input", function() {
-    if(this.value) {
-        index.search(this.value, {
-            hitsPerPage: 50
-        }).then(({hits}) => {
-            resetSearchBar(true);
-            for(let i = 0; i < Math.min(hits.length, 3); i++) {
-                let result = createResult(hits[i]["title"], hits[i]["link"]);
-                result.style["cursor"] = "pointer";
-                result.style["text-decoration"] = "underline";
-                result.addEventListener("click", function() {
-                    console.log("pressed");
-                    inputQuery.dispatchEvent(new KeyboardEvent("keypress", {'key':'Enter'}));
-                    openResult(hits[i]);
-                })
-                previewsWrapper.appendChild(result);
-            }
-            inputQuery.style["padding-bottom"] = "1vh";
-            previewsWrapper.lastChild.style["padding-bottom"] = "2vh";
-        }).catch(function() {
-            console.log("no results");
-            resetSearchBar(true);
-        });
-    } else {
+/**
+ * Creates a preview element using search query hit. When clicked, element 
+ * displays all search query results and opens the specific requested result.
+ * 
+ * @param {Object} hit Hit from search query
+ * @returns A preview element with title and link
+ */
+function generatePreview(hit) {
+    let result = createPreview(hit["title"], hit["link"]);
+    result.style["cursor"] = "pointer";
+    result.style["text-decoration"] = "underline";
+
+    // when clicked, open all results for this hit, open specific result card
+    result.addEventListener("click", function() {
+        displayGenResults();
+        openResult(hit);
+    });
+
+    // gray mouseover styling
+    result.addEventListener("mouseover", function() {
+        result.style["background-color"] = "#eeeeee";
+    });
+    result.addEventListener("mouseout", function() {
+        result.style["background-color"] = "#f9f7f7";
+    });
+
+    return result;
+}
+
+/**
+ * Searches index for value and displays 3 preview searches below search bar.
+ * 
+ * @param {string} value Current search query
+ */
+function searchDisplayPreviews(value) {
+    index.search(value, {
+        // TODO: pagination
+        hitsPerPage: 50
+    }).then(({hits}) => {
         resetSearchBar(true);
-    }
-}, false);
 
-inputQuery.addEventListener("keypress", function(e) {
-    if(e.key == "Enter") {
-        resultsWrapper.innerHTML = "";
-        sessionStorage.query = inputQuery.value;
-        //title.style["font-size"] = "5em";
+        for(let i = 0; i < Math.min(hits.length, 3); i++) {
+            let result = generatePreview(hits[i]);
+            previewsWrapper.appendChild(result);
+        }
 
-        index.search(inputQuery.value, {
-            hitsPerPage: 10
-        }).then(({hits}) => {
-            generateResultCards(hits, resultsWrapper);
-        });
-        resetSearchBar();
-    }
-});
+        // Padding changes to normalize spacing difference
+        inputQuery.style["padding-bottom"] = "1vh";
+        previewsWrapper.lastChild.style["padding-bottom"] = "2vh";
+    }).catch(function() {
+        resetSearchBar(true);
+    });
+}
 
+/**
+ * Resets results and generates new results based on query
+ * 
+ * @param {string} value Current search query
+ */
+function displayGenResults(value=inputQuery.value) {
+    resultsWrapper.innerHTML = "";
+    sessionStorage.query = value;
+    //title.style["font-size"] = "5em";
+
+    index.search(value, {
+        hitsPerPage: 10
+    }).then(({hits}) => {
+        generateResultCards(hits, resultsWrapper);
+    });
+    resetSearchBar();
+}
+
+/**
+ * Generates result cards and adds them to given wrapper based on query
+ * 
+ * @param {Object} hits Hits from search query 
+ * @param {Object} wrapper Wrapper to add result cards to
+ */
 function generateResultCards(hits, wrapper) {
     let locations = ["left", "right"];
     for(let i = 0; i < Math.min(hits.length, 10); i++) {
@@ -84,20 +155,29 @@ function generateResultCards(hits, wrapper) {
     }
 }
 
+/**
+ * Resets current result and replaces with given hit
+ * @param {string} hit Hit from search query
+ */
 function openResult(hit) {
-    console.log("results");
     currentResultWrapper.innerHTML = "";
 
     let result = createResultCard(hit, "", true);
     resultsWrapper.style["padding-top"] = "0vh";
     result.style["margin-bottom"] = "2%";
     currentResultWrapper.appendChild(result);
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+    let resultOffset = currentResultWrapper.offsetTop;
+    window.scrollTo({ top: resultOffset, behavior: 'smooth' })
 }
 
-function resetSearchBar(currentResult=false) {
+/**
+ * Resets search bar to 0 results
+ * 
+ * @param {boolean} keepCurrentResult Whether to keep current result visible
+ */
+function resetSearchBar(keepCurrentResult=false) {
     previewsWrapper.innerHTML = "";
-    if(!currentResult) {
+    if(!keepCurrentResult) {
         currentResultWrapper.innerHTML = "";
     }
     resultsWrapper.style["padding-top"] = "5vh";
@@ -105,11 +185,14 @@ function resetSearchBar(currentResult=false) {
 }
 
 /**
- * Creates h1 element with given text.
- * @param {string} text Text in h1
- * @returns HTML h1 element with given text embedded
+ * Creates preview (i.e. title element w/ or w/o link)
+ * 
+ * @param {string} text Text for preview
+ * @param {string} href Link for preview
+ * @param {boolean} embed Whether to add a link to the title
+ * @returns h1 element with text and potentially link
  */
-function createResult(text, href, embed=false) {
+function createPreview(text, href, embed=false) {
 	var result = document.createElement("h1");
     var h1A = document.createElement("a");
 	var h1Text = document.createTextNode(text);
@@ -132,8 +215,31 @@ function createResult(text, href, embed=false) {
 	return result;
 }
 
+/**
+ * Creates description and adds it to an existing result card.
+ * 
+ * @param {Object} card resultCard to add description pieces to
+ * @param {string} key Key from description
+ * @param {string} value Value of description
+ */
+function createResultDescription(card, key, value) {
+    if(key != "title" && key != "link" && key != "objectID" && typeof value == "string") {
+        let resultPiece = document.createElement("p");
+        resultPiece.appendChild(document.createTextNode(capFirstLetter(key) + ": " + value));
+        resultPiece.classList.add("card-p", "body-text");
+        card.appendChild(resultPiece);
+    }
+}
+
+/**
+ * Create a full result card with location and/or embedded audio
+ * @param {string} hit Hit from search query
+ * @param {string} location Class (left/right) to add to div
+ * @param {boolean} embed Whether to embed an audio portion
+ * @returns Result card
+ */
 function createResultCard(hit, location="", embed=false) {
-    let resultTitle = createResult(hit["title"], hit["link"], embed);
+    let resultTitle = createPreview(hit["title"], hit["link"], embed);
     resultTitle.classList.add("card-title");
     resultTitle.style["padding-bottom"] = "0vh";
 
@@ -157,12 +263,7 @@ function createResultCard(hit, location="", embed=false) {
     }
 
     for(const [key, value] of Object.entries(hit)) {
-        if(key != "title" && key != "link" && key != "objectID" && typeof value == "string") {
-            let resultPiece = document.createElement("p");
-            resultPiece.appendChild(document.createTextNode(capFirstLetter(key) + ": " + value));
-            resultPiece.classList.add("card-p", "body-text");
-            resultCard.appendChild(resultPiece);
-        }
+        createResultDescription(resultCard, key, value);
     }
 
     let tags = document.createElement("p");
@@ -173,34 +274,11 @@ function createResultCard(hit, location="", embed=false) {
     return resultCard;
 }
 
+/**
+ * Capitalizes first letter of given string
+ * @param {string} string String to capitalize
+ * @returns Capitalized string
+ */
 function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-inputQuery.addEventListener("focus", function () {
-    searchBar.style["box-shadow"] = "rgba(0, 0, 0, 0.24) 1px 1px 8px";
-});
-
-inputQuery.addEventListener("focusout", function () {
-    searchBar.style["box-shadow"] = "rgba(99, 99, 99, 0.2) 0px 0px 8px 0px";
-});
-
-function checkValidURL(link) {
-    function checkForError() {
-        var video = document.createElement("audio");
-        video.setAttribute("src", link);
-        video.addEventListener("error", function() {
-            return video.error.message;
-        });
-    }
-    /*console.log(await checkForError());
-    if(await checkForError()) {
-        return "https://www" + link.substring(10);
-    } else {
-        return link;
-    }*/
-    return checkForError();
-}
-
-//checkValidURL("https://dl.dropbox.com/s/1cni12e0tt35zfl/Texas%20Two-Step%20Swing%202%20Semifinals%20-%20McKendree%20DP%20vs%20William%20Jewell%20HU.m4a?dl=0")
-//console.log(checkValidURL("https://dl.dropbox.com/s/1cni12e0tt35zfl/Texas%20Two-Step%20Swing%202%20Semifinals%20-%20McKendree%20DP%20vs%20William%20Jewell%20HU.m4a?dl=0"));
